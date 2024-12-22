83176d0be16c271e20dbcb4fa9f687fb2ec8ae90
Zoraiz Mahmood <35128199+rzmahmood@users.noreply.github.com>
2024-12-22 11:29:52 +1100

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -3,4 +3,8 @@
 set -exu
 set -o pipefail
 
-rm -r ./config
+rm -r ./config || echo "failed to delete config dir"
+
+# Kill any hanging geth or beaconkit instances to prevent port clashes
+pkill geth || echo "No existing geth processes"
+pkill beacond || echo "No existing beaconkit processes"
